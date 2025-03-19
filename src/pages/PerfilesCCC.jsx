775fff1423d00d8c6d2008e410aae3dc06c95c7d
Wilman Conde <willconde2@hotmail.com>
2025-03-18 23:46:17 -0500

    perfiles

--- src/pages/PerfilesCCC.jsx
+++ src/pages/PerfilesCCC.jsx
@@ -1,54 +1,107 @@
 import useTitulo from '../hooks/useTitulo';
 
 const PerfilesCCC = () => {
-  useTitulo('Perfiles CCC')
+  useTitulo('Perfiles CCC');
 
   return (
     <>
-      <div className="fullContainer">
-        <div className="container">
-
-          <h1>PERFIL DEL DOCENTE COCICOR</h1>
-          <ul><li>Ser poseedor y formador de valores a través de su ejemplo en los diferentes espacios</li>
+      <div className='fullContainer'>
+        <div className='container'>
+          <h2>PERFIL DEL DIRECTIVO DOCENTE COCICOR</h2>
+          <ul>
+            <li>Ser idóneo en los aspectos pedagógicos, administrativos y de gestión que requieran de su desempeño.</li>
+            <li>
+              Poseer una sana personalidad, siendo honesto, responsable, cumplidor de su deber, amable, respetuoso,
+              justo en sus actos y procederes y con alta sensibilidad y equilibrio para la toma de las decisiones.
+            </li>
+            <li>
+              Ejercer liderazgo positivo en la planificación, organización y desarrollo de los planes, proyectos y
+              programas pedagógicos y administrativos, dirigidos a promover los cambios de mejoramiento institucional.
+            </li>
+            <li>
+              Conformar los equipos pedagógicos conducentes al análisis de situaciones y a la búsqueda de soluciones
+              propias de la práctica docente.
+            </li>
+            <li>
+              Proyectar la filosofía institucional, planificando el trabajo administrativo y docente en función de las
+              necesidades, intereses y aspiraciones de los estudiantes y comunidad educativa.
+            </li>
+          </ul>
+        </div>
+        <div className='container'>
+          <h2>PERFIL DEL DOCENTE COCICOR</h2>
+          <ul>
+            <li>Ser poseedor y formador de valores a través de su ejemplo en los diferentes espacios</li>
             <li>Ser comprometido con la formación integral (en lo personal social y cognitivo) de los estudiantes.</li>
-            <li>Ser  creativo, innovador, recursivo, colaborador y abierto al cambio.
-
+            <li>Ser creativo, innovador, recursivo, colaborador y abierto al cambio. </li>
+            <li>Poseer espíritu investigativo. </li>
+            <li>Ser justo en sus acciones y decisiones. </li>
+            <li>Mantener una buena presentación personal. </li>
+            <li>Poseer un alto sentido de pertenencia. </li>
+            <li>Ser respetuoso de las diferencias étnicas, culturales, religiosas, intelectuales y personales. </li>
+            <li>Estar dispuesto al diálogo y concertaciones. </li>
+            <li>
+              Mantener buenos relaciones interpersonales con todos los miembros de la comunidad tanto internos como
+              externos.
             </li>
-            <li>Poseer espíritu investigativo.
-
+            <li>
+              Poseer ética profesional, vocación y fundamentos pedagógicos y metodológicos para orientar el aprendizaje
+              significativo de los estudiantes.
             </li>
-            <li>Ser justo en sus acciones y decisiones
-
+            <li>Ser Idóneo y competente en su área</li>
+          </ul>
+        </div>
+        <div className='container'>
+          <h2>PERFIL DEL ESTUDIANTE COCICOR</h2>
+          <ul>
+            <li>El colegio a través de sus diversos programas busca formar personas.</li>
+            <li>Que posean y promuevan valores cívicos, sociales religiosos y ecológicos</li>
+            <li>
+              Participativos en forma crítica, reflexiva y analíticaCon espíritu de superación, liderazgo,
+              investigativo, emprendedor e innovador.
             </li>
-            <li> Mantener una buena presentación personal
-
+            <li>Respetuosos(as) de la individualidad e identidad cultual, de las normas y derechos humanos.</li>
+            <li>Compromiso permanente con la formación de sus hijos.</li>
+            <li>
+              Colaboración con el colegio en forma responsable y comprometida en la educación de sus hijos, desde la
+              perspectiva y funciones correspondientes.
             </li>
-            <li>Poseer un alto sentido de pertenencia
-
+            <li>
+              Participación en las diferentes actividades del colegio, colaborando con un espíritu positivo y solidario
+              con la institución.
             </li>
-            <li>Ser respetuoso de las diferencias étnicas, culturales, religiosas, intelectuales y personales.
-
+            <li>
+              Respetuoso de las instancias de comunicación con los diferentes estamentos, alentando, de esta manera, el
+              buen funcionamiento de la institución.
             </li>
-            <li>Estar dispuesto al diálogo y concertaciones
-
+            <li>Dispuesto a trabajar en forma colectiva para un normal desarrollo del año lectivo.</li>
+            <li>Practicante de normas democráticas, respetuosas y civilizadas en interacción social.</li>
+            <li>
+              Respetuoso de los compromisos adquiridos con el colegio, desde el inicio del año escolar, en lo económico
+              normativo y administrativo.
             </li>
-            <li>Mantener buenos relaciones interpersonales con todos
-
-              los miembros de la comunidad tanto internos como
-
-              externos.</li>
-            <li>Poseer ética profesional, vocación y fundamentos
-
-              pedagógicos y metodológicos para orientar el aprendizaje
-
-              significativo de los estudiantes.</li>
-            <li>Ser Idóneo y competente en su área
-
-            </li></ul>
+            <li>
+              Que promuevan la estabilidad y buena imagen de la institución por ser ellos parte de ésta y por el rol que
+              juegan en el buen ejemplo y valores positivos que deben mostrar ante sus hijos.
+            </li>
+            <li>Que participen responsable y democráticamente en los comités o consejos de padres y otros órganos</li>
+          </ul>
+        </div>
+        <div className='container'>
+          <h2>PERFIL DEL EGRESADO COCICOR</h2>
+          <ul>
+            <li>Componente en los diferentes campos de desempeño a nivel profesional y laboral.</li>
+            <li>
+              Convencido de que su elección profesional, será pilar fundamental para que el desarrollo social, económico
+              y científico.
+            </li>
+            <li>Ser modelo de valores, morales y profesionales.</li>
+            <li>Con permanente búsqueda de la superación y la excelencia.</li>
+          </ul>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default PerfilesCCC
+export default PerfilesCCC;
