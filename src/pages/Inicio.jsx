dc9db81a7bca2f25a633522f49d452d5ce68a9c2
Wilman Conde <willconde2@hotmail.com>
2025-03-27 13:39:52 -0500

    circular testing

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -5,36 +5,76 @@ import Formulario from '../components/Formulario';
 const Inicio = () => {
   useTitulo('Inicio');
 
-  const images = ['/background1.webp', '/background2.webp', '/background3.webp', '/background4.webp'];
+  const images = [
+    '/background1.webp',
+    '/background2.webp',
+    '/background3.webp',
+    '/background4.webp',
+  ];
 
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 5000); // 5000 ms = 5 segundos
+    }, 5000);
 
     return () => clearInterval(interval);
   }, [images]);
 
   return (
     <div className='fullContainer'>
+      {/* Fondo con carrusel de imágenes */}
       <div className='backgroundContainer'>
         {images.map((image, index) => (
-          <div key={index} className={`imageWrapper ${index === currentImageIndex ? 'active' : ''}`}>
+          <div
+            key={index}
+            className={`imageWrapper ${index === currentImageIndex ? 'active' : ''}`}
+          >
             <img src={image} alt={`background-${index}`} />
           </div>
         ))}
       </div>
 
+      {/* Texto principal */}
       <div className='backgroundContainerText'>
         <h1>Paz - Progreso - Futuro</h1>
-        <h2>¡Una comunidad acogedora que inspira posibilidades, conecta personas y estimula el aprendizaje!</h2>
+        <h2>
+          ¡Una comunidad acogedora que inspira posibilidades, conecta personas y estimula el
+          aprendizaje!
+        </h2>
       </div>
 
       <Formulario />
 
-      <div className='circular'>Circular principal</div>
+      {/* Circular principal con PDF embebido */}
+      <div className='card circular'>
+        <div className='card-body'>
+          <img src='/circular.webp' className='card-img-top' alt='...' />
+          <h5 className='card-title'>Circular Informativa</h5>
+          <p className='card-text'>
+            Ya está disponible la última Circular Informativa con novedades académicas y
+            administrativas. Puedes visualizar o descargar el documento a continuación.
+          </p>
+          <a
+            href='https://drive.google.com/uc?export=download&id=1Vq9ihhwGA2LrzNeWgr-p1AgApySRYkic'
+            className='btn btn-primary'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Descargar
+          </a>
+          <a
+            href='https://drive.google.com/drive/folders/1WRVTj1ihAB29L8p_lBO3ehNmDIGhRGwy?usp=sharing'
+            className='btn btn-primary'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            testing
+          </a>
+        </div>
+      </div>
+
       <div className='reporteAcademico'>Reporte académico</div>
       <div className='recuperacion'>Recuperación</div>
       <div className='actividadesRecuperacion'>Actividades de recuperación</div>
