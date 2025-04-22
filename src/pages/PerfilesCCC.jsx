266995210715383e0c25ba5630a8cf1540350ba9
Wilman Conde <willconde2@hotmail.com>
2025-04-21 20:43:19 -0500

    v1.1.6 photos updated

--- src/pages/PerfilesCCC.jsx
+++ src/pages/PerfilesCCC.jsx
@@ -1,156 +1,109 @@
 import useTitulo from '../hooks/useTitulo';
-import { Carrusel } from '../components/Carrusel';
+import { CarruselPerfiles } from '../components/CarruselPerfiles';
 import { useCarrusel } from '../hooks/useCarrusel';
 import {
-  BACKGROUND_IMAGESdirectivo,
-  BACKGROUND_IMAGESdocente,
-  BACKGROUND_IMAGESestudiante,
-  BACKGROUND_IMAGESegresado,
+  BACKGROUND_IMAGES_DIRECTIVO,
+  BACKGROUND_IMAGES_DOCENTE,
+  BACKGROUND_IMAGES_ESTUDIANTE,
+  BACKGROUND_IMAGES_EGRESADO,
 } from '../constants/inicio';
 
 const PerfilesCCC = () => {
   useTitulo('Perfiles CCC');
 
-  const { currentImageIndex } = useCarrusel(BACKGROUND_IMAGESdocente);
-  const { currentImageIndex: currentImageIndex2 } = useCarrusel(BACKGROUND_IMAGESdirectivo);
-  const { currentImageIndex: currentImageIndex3 } = useCarrusel(BACKGROUND_IMAGESestudiante);
-  const { currentImageIndex: currentImageIndex4 } = useCarrusel(BACKGROUND_IMAGESegresado);
+  const { currentImageIndex: indexDirectivo } = useCarrusel(BACKGROUND_IMAGES_DIRECTIVO);
+  const { currentImageIndex: indexDocente } = useCarrusel(BACKGROUND_IMAGES_DOCENTE);
+  const { currentImageIndex: indexEstudiante } = useCarrusel(BACKGROUND_IMAGES_ESTUDIANTE);
+  const { currentImageIndex: indexEgresado } = useCarrusel(BACKGROUND_IMAGES_EGRESADO);
 
   return (
-    <>
-      <div className='fullContainer containerProfiles'>
-        <div className='directivo'>
-          <Carrusel images={BACKGROUND_IMAGESdirectivo} currentImageIndex={currentImageIndex} />
-        </div>
-        <div className='container containerProfiles container containerProfilesProfiles'>
-          <h2>PERFIL DEL DIRECTIVO DOCENTE COCICOR</h2>
-          <ul>
-            <li>
-              Ser idóneo en los aspectos pedagógicos, administrativos y de gestión que requieran de
-              su desempeño.
-            </li>
-            <li>
-              Poseer una sana personalidad, siendo honesto, responsable, cumplidor de su deber,
-              amable, respetuoso, justo en sus actos y procederes y con alta sensibilidad y
-              equilibrio para la toma de las decisiones.
-            </li>
-            <li>
-              Ejercer liderazgo positivo en la planificación, organización y desarrollo de los
-              planes, proyectos y programas pedagógicos y administrativos, dirigidos a promover los
-              cambios de mejoramiento institucional.
-            </li>
-            <li>
-              Conformar los equipos pedagógicos conducentes al análisis de situaciones y a la
-              búsqueda de soluciones propias de la práctica docente.
-            </li>
-            <li>
-              Proyectar la filosofía institucional, planificando el trabajo administrativo y docente
-              en función de las necesidades, intereses y aspiraciones de los estudiantes y comunidad
-              educativa.
-            </li>
-          </ul>
-        </div>
-        <div className='docente'>
-          <Carrusel images={BACKGROUND_IMAGESdocente} currentImageIndex={currentImageIndex} />
-        </div>
-        <div className='container containerProfiles'>
-          <h2>PERFIL DEL DOCENTE COCICOR</h2>
-          <ul>
-            <li>
-              Ser poseedor y formador de valores a través de su ejemplo en los diferentes espacios
-            </li>
-            <li>
-              Ser comprometido con la formación integral (en lo personal social y cognitivo) de los
-              estudiantes.
-            </li>
-            <li>Ser creativo, innovador, recursivo, colaborador y abierto al cambio. </li>
-            <li>Poseer espíritu investigativo. </li>
-            <li>Ser justo en sus acciones y decisiones. </li>
-            <li>Mantener una buena presentación personal. </li>
-            <li>Poseer un alto sentido de pertenencia. </li>
-            <li>
-              Ser respetuoso de las diferencias étnicas, culturales, religiosas, intelectuales y
-              personales.
-            </li>
-            <li>Estar dispuesto al diálogo y concertaciones. </li>
-            <li>
-              Mantener buenos relaciones interpersonales con todos los miembros de la comunidad
-              tanto internos como externos.
-            </li>
-            <li>
-              Poseer ética profesional, vocación y fundamentos pedagógicos y metodológicos para
-              orientar el aprendizaje significativo de los estudiantes.
-            </li>
-            <li>Ser Idóneo y competente en su área</li>
-          </ul>
-        </div>
-        <div className='estudiante'>
-          <Carrusel images={BACKGROUND_IMAGESestudiante} currentImageIndex={currentImageIndex} />
-        </div>
-        <div className='container containerProfiles'>
-          <h2>PERFIL DEL ESTUDIANTE COCICOR</h2>
-          <ul>
-            <li>El colegio a través de sus diversos programas busca formar personas.</li>
-            <li>Que posean y promuevan valores cívicos, sociales religiosos y ecológicos</li>
-            <li>
-              Participativos en forma crítica, reflexiva y analíticaCon espíritu de superación,
-              liderazgo, investigativo, emprendedor e innovador.
-            </li>
-            <li>
-              Respetuosos(as) de la individualidad e identidad cultual, de las normas y derechos
-              humanos.
-            </li>
-            <li>Compromiso permanente con la formación de sus hijos.</li>
-            <li>
-              Colaboración con el colegio en forma responsable y comprometida en la educación de sus
-              hijos, desde la perspectiva y funciones correspondientes.
-            </li>
-            <li>
-              Participación en las diferentes actividades del colegio, colaborando con un espíritu
-              positivo y solidario con la institución.
-            </li>
-            <li>
-              Respetuoso de las instancias de comunicación con los diferentes estamentos, alentando,
-              de esta manera, el buen funcionamiento de la institución.
-            </li>
-            <li>
-              Dispuesto a trabajar en forma colectiva para un normal desarrollo del año lectivo.
-            </li>
-            <li>
-              Practicante de normas democráticas, respetuosas y civilizadas en interacción social.
-            </li>
-            <li>
-              Respetuoso de los compromisos adquiridos con el colegio, desde el inicio del año
-              escolar, en lo económico normativo y administrativo.
-            </li>
-            <li>
-              Que promuevan la estabilidad y buena imagen de la institución por ser ellos parte de
-              ésta y por el rol que juegan en el buen ejemplo y valores positivos que deben mostrar
-              ante sus hijos.
-            </li>
-            <li>
-              Que participen responsable y democráticamente en los comités o consejos de padres y
-              otros órganos
-            </li>
-          </ul>
-        </div>
-        <div className='egresado'>
-          <Carrusel images={BACKGROUND_IMAGESegresado} currentImageIndex={currentImageIndex} />
-        </div>
-        <div className='container containerProfiles'>
-          <h2>PERFIL DEL EGRESADO COCICOR</h2>
-          <ul>
-            <li>Componente en los diferentes campos de desempeño a nivel profesional y laboral.</li>
-            <li>
-              Convencido de que su elección profesional, será pilar fundamental para que el
-              desarrollo social, económico y científico.
-            </li>
-            <li>Ser modelo de valores, morales y profesionales.</li>
-            <li>Con permanente búsqueda de la superación y la excelencia.</li>
-          </ul>
-        </div>
+    <div className='fullContainer containerProfiles'>
+      <div className='directivo'>
+        <CarruselPerfiles
+          images={BACKGROUND_IMAGES_DIRECTIVO}
+          currentImageIndex={indexDirectivo}
+          backgroundColor='#E0F7FA'
+        />
       </div>
-    </>
+      <div className='container containerProfiles'>
+        <h2>PERFIL DEL DIRECTIVO DOCENTE COCICOR</h2>
+        <ul>
+          <li>Ser idóneo en los aspectos pedagógicos, administrativos y de gestión.</li>
+          <li>Poseer una sana personalidad, siendo honesto, responsable.</li>
+          <li>Ejercer liderazgo positivo en la planificación.</li>
+          <li>Conformar los equipos pedagógicos conducentes al análisis.</li>
+          <li>Proyectar la filosofía institucional.</li>
+        </ul>
+      </div>
+
+      <div className='docente'>
+        <CarruselPerfiles
+          images={BACKGROUND_IMAGES_DOCENTE}
+          currentImageIndex={indexDocente}
+          backgroundColor='#FFF3E0'
+        />
+      </div>
+      <div className='container containerProfiles'>
+        <h2>PERFIL DEL DOCENTE COCICOR</h2>
+        <ul>
+          <li>Ser poseedor y formador de valores a través de su ejemplo.</li>
+          <li>Ser comprometido con la formación integral.</li>
+          <li>Ser creativo, innovador, recursivo, colaborador.</li>
+          <li>Poseer espíritu investigativo.</li>
+          <li>Ser justo en sus acciones y decisiones.</li>
+          <li>Mantener una buena presentación personal.</li>
+          <li>Poseer un alto sentido de pertenencia.</li>
+          <li>Ser respetuoso de las diferencias.</li>
+          <li>Estar dispuesto al diálogo y concertaciones.</li>
+          <li>Mantener buenas relaciones interpersonales.</li>
+          <li>Poseer ética profesional, vocación y fundamentos pedagógicos.</li>
+          <li>Ser idóneo y competente en su área.</li>
+        </ul>
+      </div>
+
+      <div className='estudiante'>
+        <CarruselPerfiles
+          images={BACKGROUND_IMAGES_ESTUDIANTE}
+          currentImageIndex={indexEstudiante}
+          backgroundColor='#F1F8E9'
+        />
+      </div>
+      <div className='container containerProfiles'>
+        <h2>PERFIL DEL ESTUDIANTE COCICOR</h2>
+        <ul>
+          <li>El colegio a través de sus diversos programas busca formar personas.</li>
+          <li>Que posean y promuevan valores cívicos, sociales religiosos y ecológicos.</li>
+          <li>Participativos en forma crítica, reflexiva y analítica.</li>
+          <li>Respetuosos(as) de la individualidad e identidad cultural.</li>
+          <li>Compromiso permanente con la formación de sus hijos.</li>
+          <li>Colaboración con el colegio en forma responsable.</li>
+          <li>Participación en las diferentes actividades del colegio.</li>
+          <li>Respetuoso de las instancias de comunicación.</li>
+          <li>Dispuesto a trabajar en forma colectiva.</li>
+          <li>Practicante de normas democráticas.</li>
+          <li>Respetuoso de los compromisos adquiridos con el colegio.</li>
+          <li>Que promuevan la estabilidad y buena imagen.</li>
+          <li>Que participen responsable y democráticamente.</li>
+        </ul>
+      </div>
+
+      <div className='egresado'>
+        <CarruselPerfiles
+          images={BACKGROUND_IMAGES_EGRESADO}
+          currentImageIndex={indexEgresado}
+          backgroundColor='#F5DBD8'
+        />
+      </div>
+      <div className='container containerProfiles'>
+        <h2>PERFIL DEL EGRESADO COCICOR</h2>
+        <ul>
+          <li>Componente en los diferentes campos de desempeño a nivel profesional y laboral.</li>
+          <li>Convencido de que su elección profesional será pilar fundamental.</li>
+          <li>Ser modelo de valores, morales y profesionales.</li>
+          <li>Con permanente búsqueda de la superación y la excelencia.</li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
